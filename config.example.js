2edffcb9b05d1c0db3957b4f94de5c37b576db62
Francesc Rosàs <francescrosasbosque@gmail.com>
2017-02-10 17:56:49 +0000

    Make mailboxes to backup part of the config

--- config.example.js
+++ config.example.js
@@ -1,12 +1,13 @@
 module.exports = {
+  // Connection and authentication options as passed to node-imap (see available
+  // options at https://github.com/mscdex/node-imap#connection-instance-methods).
   imap: {
-    // See https://github.com/mscdex/node-imap#connection-instance-methods for 
-    // rest of options
     user: '<user>',
     password: '<password>',
     host: '<host>',
     port: 993,
     tls: true,
     authTimeout: 60000
-  }
+  },
+  mailboxes: ['INBOX']
 }
